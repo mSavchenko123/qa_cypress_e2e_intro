1d2ab90acfb1dc7a186c0d581fab31cfafd8f18d
MaksSavchenko <savchenkomaks96@gmail.com>
2025-05-09 20:20:13 +0300

    fix: update sign-in test with user object

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,17 +1,23 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const user = {
+    email: 'test123321@i.ua',
+    username: 'test123321',
+    password: 'testpassword'
+  };
+
   it('should provide an ability to log in', () => {
     cy.visit('https://conduit.mate.academy/user/login');
 
     cy.get('[placeholder=Email]')
-      .type('testexampleemail@gmail.com');
+      .type(user.email);
     cy.get('[placeholder=Password]')
-      .type('testpassword');
+      .type(user.password);
 
     cy.get('.btn').click();
 
     cy.get('a.nav-link')
-      .should('contain.text', 'usertestacc');
+      .should('contain.text', user.username);
   });
 });

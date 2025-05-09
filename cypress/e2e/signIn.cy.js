80762b581b4cd8f9f2ad4e63cfdbcf7b93c1ffff
MaksSavchenko <savchenkomaks96@gmail.com>
2025-05-09 14:39:23 +0300

    Implement sign-in test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/user/login');
 
+    cy.get('[placeholder=Email]')
+      .type('testexampleemail@gmail.com');
+    cy.get('[placeholder=Password]')
+      .type('testpassword');
+
+    cy.get('.btn').click();
+
+    cy.get('a.nav-link')
+      .should('contain.text', 'usertestacc');
   });
 });
